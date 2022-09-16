dad0bfcb9dcdfdf6ed208665bbd8bb3994fa7a9e
Jatin <arorajatin6@gmail.com>
2022-09-16 14:16:41 +0530

    feat: added base error handlers for api

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 
 from sqlalchemy.ext.asyncio import create_async_engine
 from starlette.middleware.cors import CORSMiddleware
 
 from app.core.settings.app import AppSettings
+from app.api.errors import http_error, validation_error
 
 def get_application(settings: AppSettings) -> FastAPI:
     
@@ -20,4 +23,8 @@ def get_application(settings: AppSettings) -> FastAPI:
     @app.on_event("shutdown")
     def disconnect_from_db():
         app.state.engine.close()
+
+
+    app.add_exception_handler(HTTPException, http_error.http_error_handler)
+    app.add_exception_handler(RequestValidationError, validation_error.http422_error_handler)
     return app
